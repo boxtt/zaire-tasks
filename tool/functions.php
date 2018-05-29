27ca3d7357059e90cea1602f76797c157cf55b2a
tgz2017 <1058919321@qq.com>
2018-05-29 15:15:52 +0800

    图片缩放函数

--- tool/functions.php
+++ tool/functions.php
@@ -2377,3 +2377,100 @@ function get_express_100($num)
 	$data = getSslPage($url2);
 	$re2  = json_decode($data, TRUE);
 }
+
+/**
+ * 图片缩放函数（可设置高度固定，宽度固定或者最大宽高，支持gif/jpg/png三种类型）
+ * Author : Specs
+ * Homepage: https://9iphp.com
+ *
+ * @param string $source_path   源图片
+ * @param string $target_path   新图片要保存的地址，全路径，包括名字
+ * @param int    $target_width  目标宽度
+ * @param int    $target_height 目标高度
+ * @param string $fixed_orig    锁定宽高（可选参数 width、height或者空值）
+ *
+ * @return string
+ */
+function image_resize($source_path, $target_path, $target_width = 200, $target_height = 200, $fixed_orig = '')
+{
+	$source_info   = getimagesize($source_path);
+	$source_width  = $source_info[0];
+	$source_height = $source_info[1];
+	$source_mime   = $source_info['mime'];
+	$ratio_orig    = $source_width / $source_height;
+	if ($fixed_orig == 'width')
+	{
+		//宽度固定
+		$target_height = $target_width / $ratio_orig;
+	}
+	elseif ($fixed_orig == 'height')
+	{
+		//高度固定
+		$target_width = $target_height * $ratio_orig;
+	}
+	else
+	{
+		//最大宽或最大高
+		if ($target_width / $target_height > $ratio_orig)
+		{
+			$target_width = $target_height * $ratio_orig;
+		}
+		else
+		{
+			$target_height = $target_width / $ratio_orig;
+		}
+	}
+	switch ($source_mime)
+	{
+		case 'image/gif':
+			$source_image = imagecreatefromgif($source_path);
+			break;
+
+		case 'image/jpeg':
+			$source_image = imagecreatefromjpeg($source_path);
+			break;
+
+		case 'image/png':
+			$source_image = imagecreatefrompng($source_path);
+			break;
+
+		default:
+			return FALSE;
+			break;
+	}
+	$target_image = imagecreatetruecolor($target_width, $target_height);
+	imagecopyresampled($target_image, $source_image, 0, 0, 0, 0, $target_width, $target_height, $source_width,
+		$source_height);
+//	header('Content-type: image/jpeg');
+	imagejpeg($target_image, $target_path, 100);
+	return TRUE;
+}
+
+//参数images为图片的绝对地址
+function get_images_info($images)
+{
+	$img_info = getimagesize($images);
+	switch ($img_info[2])
+	{
+		case 1:
+			$imgtype = "gif";
+			break;
+		case 2:
+			$imgtype = "jpg";
+			break;
+		case 3:
+			$imgtype = "png";
+			break;
+	}
+	$img_type = $imgtype."图像";
+//获取文件大小
+	$img_size     = ceil(filesize($images) / 1000)."k";
+	$new_img_info = array(
+		"width"  => $img_info[0], //图像宽
+		"height" => $img_info[1], //图像高
+		"type"   => $img_type, //图像类型
+		"size"   => $img_size //图像大小
+	);
+
+	return $new_img_info;
+}
